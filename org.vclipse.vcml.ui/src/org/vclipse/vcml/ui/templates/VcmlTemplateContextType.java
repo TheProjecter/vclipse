6b7b94b890a3ab7af6d618fdb887143328480fb3
a_shumilin <a_shumilin@gmx.de>
2012-04-18 12:51:44 +0000

    code adaptations for graph viewer

---
+++ org.vclipse.vcml.ui/src/org/vclipse/vcml/ui/templates/VcmlTemplateContextType.java
@@ -0,0 +1,18 @@
+package org.vclipse.vcml.ui.templates;
+
+import org.eclipse.xtext.ui.editor.templates.XtextTemplateContextType;
+
+import com.google.inject.Inject;
+
+public class VcmlTemplateContextType extends XtextTemplateContextType {
+
+	@Inject
+	public void setDomainValuesResolver(ChoiceVariableResolver resolver) {
+		addResolver(resolver);
+	}
+	
+	@Inject
+	public void setFilteredCrossReferenceResolver(FilteringCrossReferenceResolver resolver) {
+		addResolver(resolver);
+	}
+}
