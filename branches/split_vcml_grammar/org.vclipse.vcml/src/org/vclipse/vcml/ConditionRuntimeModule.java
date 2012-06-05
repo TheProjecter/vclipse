935789667bc3b90cd140b5eaab9baf66fa3dc8c4
a_shumilin@gmx.de <a_shumilin@gmx.de@13a01e8a-3cdf-48c0-2aaf-27d1ace07525>
2012-06-05 12:18:00 +0000

    introduced dummy ContainerStateProvider

--- branches/split_vcml_grammar/org.vclipse.vcml/src/org/vclipse/vcml/ConditionRuntimeModule.java
+++ branches/split_vcml_grammar/org.vclipse.vcml/src/org/vclipse/vcml/ConditionRuntimeModule.java
@@ -3,9 +3,14 @@
  */
 package org.vclipse.vcml;
 
+import org.vclipse.vcml.resource.ContainerStateProvider;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class ConditionRuntimeModule extends org.vclipse.vcml.AbstractConditionRuntimeModule {
 
+	public Class<? extends org.eclipse.xtext.resource.containers.IAllContainersState.Provider> bindIAllContainersState$Provider() {
+		return ContainerStateProvider.class;
+	}
 }
