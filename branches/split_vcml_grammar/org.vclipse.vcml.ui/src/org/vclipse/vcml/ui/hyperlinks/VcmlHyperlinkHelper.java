982b62c2c2438640e85ed4c99578920ad9e03ea0
a_shumilin@gmx.de <a_shumilin@gmx.de@13a01e8a-3cdf-48c0-2aaf-27d1ace07525>
2012-06-06 10:37:42 +0000

    minor enhancement: only names are shown like hyperlinks

--- branches/split_vcml_grammar/org.vclipse.vcml.ui/src/org/vclipse/vcml/ui/hyperlinks/VcmlHyperlinkHelper.java
+++ branches/split_vcml_grammar/org.vclipse.vcml.ui/src/org/vclipse/vcml/ui/hyperlinks/VcmlHyperlinkHelper.java
@@ -1,14 +1,18 @@
 package org.vclipse.vcml.ui.hyperlinks;
 
+import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.text.Region;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.EObjectAtOffsetHelper;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.hyperlinking.HyperlinkHelper;
 import org.eclipse.xtext.ui.editor.hyperlinking.IHyperlinkAcceptor;
-import org.eclipse.xtext.util.SimpleAttributeResolver;
 import org.vclipse.vcml.resource.VCObjectSourceUtils;
 import org.vclipse.vcml.vcml.Procedure;
+import org.vclipse.vcml.vcml.VcmlPackage;
 
 import com.google.inject.Inject;
 
@@ -27,7 +31,9 @@ public class VcmlHyperlinkHelper extends HyperlinkHelper {
 		if(elementAt instanceof Procedure) {
 			EObject source = sourceUtils.getSource(elementAt);
 			if(source != null) {
-				createHyperlinksTo(resource, new Region(offset, SimpleAttributeResolver.NAME_RESOLVER.apply(elementAt).length()), source, acceptor);				
+				List<INode> findNodesForFeature = NodeModelUtils.findNodesForFeature(elementAt, VcmlPackage.eINSTANCE.getVCObject_Name());
+				INode nameNode = findNodesForFeature.get(0);
+				createHyperlinksTo(resource, new Region(nameNode.getTotalOffset(), nameNode.getTotalLength()), source, acceptor);				
 			}
 		}
 	}
