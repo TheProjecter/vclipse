b5688cbe258ccf7016196681ba2a455b2b08eb54
a_shumilin@gmx.de <a_shumilin@gmx.de@13a01e8a-3cdf-48c0-2aaf-27d1ace07525>
2012-06-11 14:03:05 +0000

    extended with a method

--- branches/split_vcml_grammar/org.vclipse.base.ui/src/org/vclipse/base/ui/util/VClipseResourceUtil.java
+++ branches/split_vcml_grammar/org.vclipse.base.ui/src/org/vclipse/base/ui/util/VClipseResourceUtil.java
@@ -3,12 +3,17 @@ package org.vclipse.base.ui.util;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.ui.util.ResourceUtil;
 
 public class VClipseResourceUtil extends ResourceUtil {
 
 	private XtextResourceSet defaultResourceSet;
+	
+	public ResourceSet getResourceSet() {
+		return defaultResourceSet;
+	}
 	
 	public Resource getResource(XtextResourceSet resourceSet, IFile file) {
 		if(resourceSet == null) {
