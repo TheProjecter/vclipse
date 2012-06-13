aff491c376a090a3b05f8e5e08e7b811e49fb32a
tg@webxcerpt.com <tg@webxcerpt.com@13a01e8a-3cdf-48c0-2aaf-27d1ace07525>
2012-06-13 14:12:46 +0000

    fixed text of hyperlink to dependency source

--- branches/split_vcml_grammar/org.vclipse.vcml.ui/src/org/vclipse/vcml/ui/hyperlinks/VcmlHyperlinkHelper.java
+++ branches/split_vcml_grammar/org.vclipse.vcml.ui/src/org/vclipse/vcml/ui/hyperlinks/VcmlHyperlinkHelper.java
@@ -2,7 +2,10 @@ package org.vclipse.vcml.ui.hyperlinks;
 
 import java.util.List;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.text.Region;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
@@ -10,6 +13,7 @@ import org.eclipse.xtext.resource.EObjectAtOffsetHelper;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.hyperlinking.HyperlinkHelper;
 import org.eclipse.xtext.ui.editor.hyperlinking.IHyperlinkAcceptor;
+import org.eclipse.xtext.ui.editor.hyperlinking.XtextHyperlink;
 import org.vclipse.vcml.utils.DependencySourceUtils;
 import org.vclipse.vcml.vcml.Dependency;
 import org.vclipse.vcml.vcml.VcmlPackage;
@@ -34,9 +38,22 @@ public class VcmlHyperlinkHelper extends HyperlinkHelper {
 				List<INode> findNodesForFeature = NodeModelUtils.findNodesForFeature(elementAt, VcmlPackage.eINSTANCE.getVCObject_Name());
 				if(!findNodesForFeature.isEmpty()) {
 					INode nameNode = findNodesForFeature.get(0);
-					createHyperlinksTo(resource, new Region(nameNode.getTotalOffset() + 1, nameNode.getTotalLength()), source, acceptor);					
+					createHyperlinksToSource(resource, new Region(nameNode.getTotalOffset() + 1, nameNode.getTotalLength()), source, acceptor);					
 				}
 			}
 		}
 	}
+	
+	protected void createHyperlinksToSource(XtextResource from, Region region, EObject to, IHyperlinkAcceptor acceptor) {
+		final URIConverter uriConverter = from.getResourceSet().getURIConverter();
+		final URI uri = EcoreUtil.getURI(to);
+		final URI normalized = uriConverter.normalize(uri);
+
+		XtextHyperlink result = getHyperlinkProvider().get();
+		result.setHyperlinkRegion(region);
+		result.setURI(normalized);
+		result.setHyperlinkText("Go to dependency source");
+		acceptor.accept(result);
+	}
+
 }
