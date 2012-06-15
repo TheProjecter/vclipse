5014f297bb2ef13a1feef73e7e69edd9637816cb
a_shumilin@gmx.de <a_shumilin@gmx.de@13a01e8a-3cdf-48c0-2aaf-27d1ace07525>
2012-06-15 11:28:03 +0000

    enahancements in the proxy resolver

--- branches/split_vcml_grammar/org.vclipse.vcml.ui/src/org/vclipse/vcml/ui/outline/actions/SapProxyResolver.java
+++ branches/split_vcml_grammar/org.vclipse.vcml.ui/src/org/vclipse/vcml/ui/outline/actions/SapProxyResolver.java
@@ -2,15 +2,19 @@ package org.vclipse.vcml.ui.outline.actions;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Collection;
 import java.util.Set;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.CrossReference;
+import org.eclipse.xtext.GrammarUtil;
+import org.eclipse.xtext.nodemodel.BidiIterator;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.vclipse.vcml.ui.extension.IExtensionPointUtilities;
+import org.vclipse.vcml.vcml.VcmlPackage;
 
 import com.google.inject.Inject;
 
@@ -20,24 +24,32 @@ public class SapProxyResolver {
 	private IExtensionPointUtilities extensionPointReader;
 	
 	public void resolveProxies(EObject object, Set<String> seenObjects, Resource output) {
-		TreeIterator<EObject> treeIterator = object.eAllContents();
-		while(treeIterator.hasNext()) {
-			for(EObject crossReference : treeIterator.next().eCrossReferences()) {
-				if(crossReference.eIsProxy()) {
-					Collection<IVcmlOutlineActionHandler<?>> handlers = extensionPointReader.getHandler(crossReference.eClass().getInstanceClassName());
-					for(IVcmlOutlineActionHandler<?> handler : handlers) {
-						if(handler.getClass().getSimpleName().contains("Extract")) {
-							try {
-								Method method = handler.getClass().getMethod("run", new Class[]{getInstanceType(crossReference), Resource.class, IProgressMonitor.class, Set.class});
-								method.invoke(handler, new Object[]{crossReference, output == null ? object.eResource() : output, new NullProgressMonitor(), seenObjects});
-							} catch(InvocationTargetException exception) {
-								if(exception.getTargetException() instanceof OutlineActionCanceledException) {
-									break;
-								} else {
+		BidiIterator<INode> iterator = NodeModelUtils.getNode(object).getChildren().iterator();
+		while(iterator.hasNext()) {
+			iterator = iterator.next().getAsTreeIterable().iterator();
+			while(iterator.hasNext()) {
+				INode childNode = iterator.next();
+				EObject grammarElement = childNode.getGrammarElement();
+				if(grammarElement instanceof CrossReference) {
+					EObject semanticElement = childNode.getSemanticElement();
+					Object referenceValue = semanticElement.eGet(GrammarUtil.getReference((CrossReference)grammarElement, semanticElement.eClass()));
+					if(referenceValue instanceof EObject) {
+						EObject eobject = (EObject)referenceValue;
+						eobject.eSet(VcmlPackage.eINSTANCE.getVCObject_Name(), childNode.getText());
+						for(IVcmlOutlineActionHandler<?> handler : extensionPointReader.getHandler(eobject.eClass().getInstanceClassName())) {
+							if(handler.getClass().getSimpleName().contains("Extract")) {
+								try {
+									Method method = handler.getClass().getMethod("run", new Class[]{getInstanceType(eobject), Resource.class, IProgressMonitor.class, Set.class});
+									method.invoke(handler, new Object[]{eobject, output == null ? object.eResource() : output, new NullProgressMonitor(), seenObjects});
+								} catch(InvocationTargetException exception) {
+									if(exception.getTargetException() instanceof OutlineActionCanceledException) {
+										break;
+									} else {
+										exception.printStackTrace();
+									}
+								} catch(Exception exception) {
 									exception.printStackTrace();
 								}
-							} catch(Exception exception) {
-								exception.printStackTrace();
 							}
 						}
 					}
