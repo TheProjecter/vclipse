44d3bd2517f52601e66986c05686dfdfcc6f2b14
a_shumilin@gmx.de <a_shumilin@gmx.de@13a01e8a-3cdf-48c0-2aaf-27d1ace07525>
2012-06-25 09:55:41 +0000

    enhancements in the sap proxy resolver

--- branches/split_vcml_grammar/org.vclipse.vcml.ui/src/org/vclipse/vcml/ui/outline/actions/SapProxyResolver.java
+++ branches/split_vcml_grammar/org.vclipse.vcml.ui/src/org/vclipse/vcml/ui/outline/actions/SapProxyResolver.java
@@ -9,9 +9,11 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.AbstractElement;
 import org.eclipse.xtext.CrossReference;
 import org.eclipse.xtext.GrammarUtil;
-import org.eclipse.xtext.nodemodel.BidiIterator;
+import org.eclipse.xtext.nodemodel.BidiTreeIterator;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.vclipse.vcml.ui.extension.IExtensionPointUtilities;
@@ -26,31 +28,36 @@ public class SapProxyResolver {
 	private IExtensionPointUtilities extensionPointReader;
 	
 	public void resolveProxies(EObject object, Set<String> seenObjects, Resource output, List<Option> options) {
-		BidiIterator<INode> iterator = NodeModelUtils.getNode(object).getChildren().iterator();
-		while(iterator.hasNext()) {
-			iterator = iterator.next().getAsTreeIterable().iterator();
-			while(iterator.hasNext()) {
-				INode childNode = iterator.next();
+		ICompositeNode node = NodeModelUtils.getNode(object);
+		if(node != null) {
+			for(INode childNode : node.getChildren()) {
 				EObject grammarElement = childNode.getGrammarElement();
-				if(grammarElement instanceof CrossReference) {
-					EObject semanticElement = childNode.getSemanticElement();
-					Object referenceValue = semanticElement.eGet(GrammarUtil.getReference((CrossReference)grammarElement, semanticElement.eClass()));
-					if(referenceValue instanceof EObject) {
-						EObject eobject = (EObject)referenceValue;
-						eobject.eSet(VcmlPackage.eINSTANCE.getVCObject_Name(), childNode.getText().trim());
-						for(IVcmlOutlineActionHandler<?> handler : extensionPointReader.getHandler(eobject.eClass().getInstanceClassName())) {
-							if(handler.getClass().getSimpleName().contains("Extract")) {
-								try {
-									Method method = handler.getClass().getMethod("run", new Class[]{getInstanceType(eobject), Resource.class, IProgressMonitor.class, Set.class, List.class});
-									method.invoke(handler, new Object[]{eobject, output == null ? object.eResource() : output, new NullProgressMonitor(), seenObjects, options});
-								} catch(InvocationTargetException exception) {
-									if(exception.getTargetException() instanceof OutlineActionCanceledException) {
-										break;
-									} else {
-										exception.printStackTrace();
+				if(grammarElement instanceof AbstractElement) {
+					BidiTreeIterator<INode> iterator = childNode.getAsTreeIterable().iterator();
+					while(iterator.hasNext()) {
+						INode nextLevelChildNode = iterator.next();
+						EObject nextLevelGrammarElement = nextLevelChildNode.getGrammarElement();
+						if(nextLevelGrammarElement instanceof CrossReference) {
+							EObject semanticElement = nextLevelChildNode.getSemanticElement();
+							Object referenceValue = semanticElement.eGet(GrammarUtil.getReference((CrossReference)nextLevelGrammarElement, semanticElement.eClass()));
+							if(referenceValue instanceof EObject) {
+								EObject eobject = (EObject)referenceValue;
+								eobject.eSet(VcmlPackage.eINSTANCE.getVCObject_Name(), nextLevelChildNode.getText().trim());
+								for(IVcmlOutlineActionHandler<?> handler : extensionPointReader.getHandler(eobject.eClass().getInstanceClassName())) {
+									if(handler.getClass().getSimpleName().contains("Extract")) {
+										try {
+											Method method = handler.getClass().getMethod("run", new Class[]{getInstanceType(eobject), Resource.class, IProgressMonitor.class, Set.class, List.class});
+											method.invoke(handler, new Object[]{eobject, output == null ? object.eResource() : output, new NullProgressMonitor(), seenObjects, options});
+										} catch(InvocationTargetException exception) {
+											if(exception.getTargetException() instanceof OutlineActionCanceledException) {
+												break;
+											} else {
+												exception.printStackTrace();
+											}
+										} catch(Exception exception) {
+											exception.printStackTrace();
+										}
 									}
-								} catch(Exception exception) {
-									exception.printStackTrace();
 								}
 							}
 						}
