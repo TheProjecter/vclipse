4958b3509806802f597f1c59876e6912fec9bcc8
a_shumilin@gmx.de <a_shumilin@gmx.de@13a01e8a-3cdf-48c0-2aaf-27d1ace07525>
2012-07-18 10:22:20 +0000

    changed vcml meta model: bom, configuration profile objects are top level objects now

---
+++ branches/modelchange_top_level_objects/org.vclipse.vcml.ui.actions/src/org/vclipse/bapi/actions/varianttable/content/VariantTableContentDisplayActionHandler.java
@@ -0,0 +1,24 @@
+package org.vclipse.bapi.actions.varianttable.content;
+
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.vclipse.bapi.actions.IBAPIActionRunner;
+import org.vclipse.vcml.vcml.Option;
+import org.vclipse.vcml.vcml.VariantTableContent;
+import org.vclipse.vcml.vcml.VcmlModel;
+
+public class VariantTableContentDisplayActionHandler extends VariantTableContentReader implements IBAPIActionRunner<VariantTableContent> {
+
+	@Override
+	public boolean isEnabled(VariantTableContent object) {
+		return isConnected();
+	}
+	
+	@Override
+	public void run(VariantTableContent variantTableContent, Resource resource,IProgressMonitor monitor, Set<String> seenObjects, List<Option> options) throws Exception {
+		read(variantTableContent.getName(), (VcmlModel)resource.getContents().get(0), monitor, seenObjects, options, false);
+	}
+}
